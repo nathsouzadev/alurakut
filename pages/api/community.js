e44809a1200747bf4d9b1a0e00ade38a3c123baa
Nathally Souza <nathallyccd@gmail.com>
2021-07-16 10:20:02 -0300

    Fix request community

--- pages/api/community.js
+++ pages/api/community.js
@@ -1,5 +1,5 @@
 export default async function community(request, response) {
-    const TOKEN = process.env.DATA_TOKEN
+    const TOKEN = '441f486af29e86d7a55f5045bd9571'
     
     await fetch(
         'https://graphql.datocms.com/',
