c02e88c78bda5846497e82e779099da6f693bb36
Nathally Souza <nathallyccd@gmail.com>
2021-07-16 09:37:18 -0300

    Fix CORS

--- pages/api/create-community.js
+++ pages/api/create-community.js
@@ -1,7 +1,15 @@
 import { SiteClient } from 'datocms-client';
 
 export default async function community(request, response) {
-    const TOKEN = '4b3a91908f2ed2e44fd7ce484074a2';
+  response.setHeader('Access-Control-Allow-Credentials', true)
+  response.setHeader('Access-Control-Allow-Origin', '*')
+  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  response.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  ) 
+  
+  const TOKEN = '4b3a91908f2ed2e44fd7ce484074a2';
     const client = new SiteClient(TOKEN);
 
     const { title, imageUrl, communityUrl } = request.body;
